eda08d48ae9f066d02d445f40f6f35d93207342c
AY02 <alessioyang.02@gmail.com>
2026-05-15 01:16:17 +0200

    Refactoring code.

--- projects/apps.py
+++ projects/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class ProjectsConfig(AppConfig):
   name = 'projects'
+  
   def ready(self):
     # Import the signals so that the framework can register them on startup.
     import projects.signals
