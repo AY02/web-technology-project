be05e3b5a9e11206a4699fa0d9ae2b9c98780391
AY02 <alessioyang.02@gmail.com>
2026-05-15 11:28:59 +0200

    Finishing auth views and performing migrations.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# Where are LoginView and LogoutView?
+# We don’t define them here! Our urls.py uses django.contrib.auth.urls,
+# which automatically loads the framework’s default views.
+
+
+class SignUpView(generic.CreateView):
+  form_class = UserCreationForm
+  success_url = reverse_lazy("login")
+  template_name = "accounts/register.html"
+
+@login_required
+def profile_view(request):
+  return render(request, 'accounts/profile.html')
