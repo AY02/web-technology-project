e3b7b90f3443b23523e5cbb904dd921bb1a3cdcd
AY02 <alessioyang.02@gmail.com>
2026-05-18 13:46:15 +0200

    Adding ProjectPermission to the admin.py file after removing the composite primary key.

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, ProjectPermission
 
 
 admin.site.register(Project)
-
-# In Django version 6.0.5, the admin cannot save models with composite primary
-# keys.
-# admin.site.register(ProjectPermission)
+admin.site.register(ProjectPermission)
