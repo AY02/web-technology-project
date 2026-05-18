57599fafff13ffbe62c8e6fa57be3af1202e15c2
AY02 <alessioyang.02@gmail.com>
2026-05-18 11:40:35 +0200

    Implementing comment model.

--- almost_fs/settings.py
+++ almost_fs/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'accounts',
     'documents',
     'projects',
-    'todos'
+    'todos',
+    'comments'
 ]
 
 MIDDLEWARE = [
---
+++ comments/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.conf import settings
+
+
+class Comment(models.Model):
+  # CASCADE: If we delete a project, we also delete the comments associated with it.
+  project = models.ForeignKey(
+    'projects.Project', 
+    on_delete=models.CASCADE,
+    related_name='comments'
+  )
+  # SET_NULL: If we delete an user, we do not delete their comments; instead, we set
+  # the attribute to NULL.
+  user = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=True,
+    related_name='user_comments'
+  )
+
+  title = models.CharField(max_length=128)
+  content = models.TextField()
+
+  creation_date = models.DateTimeField(auto_now_add=True)
+  last_updated_date = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    # By default, we sort from newest to oldest.
+    ordering = ['-creation_date']
+
+  def __str__(self):
+    author = self.user.username if self.user else "User deleted"
+    return f"Comment of [{author}] {self.title} (Project: {self.project.title})"
