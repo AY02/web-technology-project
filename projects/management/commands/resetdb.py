d6243bd6b67fe8cb219e7cf5ce70d5b9f7cab2ad
AY02 <alessioyang.02@gmail.com>
2026-06-19 18:07:15 +0200

    Correcting User class in resetdb command.

--- projects/management/commands/resetdb.py
+++ projects/management/commands/resetdb.py
@@ -1,7 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from projects.models import Project, ProjectPermission
+
+
+User = get_user_model()
 
 
 class Command(BaseCommand):
