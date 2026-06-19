439ce25126f9bc163a9ee2e0793deac39eea096a
AY02 <alessioyang.02@gmail.com>
2026-06-19 13:49:46 +0200

    Moving todolist unique constraint from form to model

--- todos/forms.py
+++ todos/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import ToDoEntry
 
+
 class ToDoEntryForm(forms.ModelForm):
   def __init__(self, *args, **kwargs):
     self.todo_list = kwargs.pop('todo_list', None)
@@ -20,22 +21,3 @@ class ToDoEntryForm(forms.ModelForm):
         'type': 'date'
       }),
     }
-    
-  def clean(self):
-    cleaned_data = super().clean()
-    content = cleaned_data.get("content")
-    deadline = cleaned_data.get("deadline")
-
-    # Check anti duplicates
-    if self.todo_list and content:
-      same_name = ToDoEntry.objects.filter(
-        todo=self.todo_list,
-        content=content
-        )
-      
-      if self.instance and self.instance.pk:
-        same_name = same_name.exclude(pk=self.instance.pk)
-      if same_name.exists():
-        raise forms.ValidationError("A task with this name already exists in this list.")
-   
-    return cleaned_data
