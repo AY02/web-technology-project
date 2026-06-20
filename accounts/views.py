1efc542c401f9661f8e27fde986faf46eaeb6006
AY02 <alessioyang.02@gmail.com>
2026-06-21 00:37:33 +0200

    Correcting register form after using custom user.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
@@ -12,7 +12,7 @@ from django.contrib.auth import login
 
 
 class SignUpView(generic.CreateView):
-  form_class = UserCreationForm
+  form_class = CustomUserCreationForm
   success_url = reverse_lazy('profile')
   template_name = 'accounts/register.html'
 
